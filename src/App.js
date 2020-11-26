c36c6a3f2efac8a9ac940710e02ef40be8fbaf76
Melkisedek Ubalde <melkisedek222@gmail.com>
2020-11-26 09:57:32 +0800

    commit1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
+    // BEM Convension
     <div className="App">
         <h1>Hello </h1>
+        {/* HEADER */}
+        {/* SIDEBAR */}
+        {/* RECOMMENDED VIDEO */}
     </div>
   );
 }
+
+
 
 export default App;
